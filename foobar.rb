b5e2bfad74037cf5956489cc84adee1b5415954a
Gabi Dumaguin <gabrianna@berkeley.edu>
2015-09-16 17:52:52 -0700

    First submission

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,19 @@
 class Foobar
-  # Q4 CODE HERE
+  
+	def self.baz(a)
+		arr = a.map{ |x| x.to_i }
+		new_arr = []
+		ints = Hash.new
+		for i in 0..arr.length-1
+			if not ints[arr[i]]
+				ints[arr[i]] = true
+				if arr[i] % 2 == 0 and arr[i] <= 8
+					new_arr.push(arr[i]+2)
+				end
+			end
+		end
+		sum = 0
+		return new_arr.inject{ |sum,x| sum+x }
+	end
+
 end
--- hw1.rb
+++ hw1.rb
@@ -1,17 +1,20 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
+	(a + b) ** 2
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
+	arr = a.map{ |x| x + 1 }.sort
+	return arr
 end
 
+# puts sort_array_plus_one [49, 29, 59, 99, -1, 39, 19, 9, 89, 69, 79]
+
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
+	return first_name + " " + last_name
 end
 
 def blockin_time(a)
-  # DO NOT EDIT THIS CODE BELOW
-  require './foobar'
-  Foobar.baz a
+	# DO NOT EDIT THIS CODE BELOW
+	require './foobar'
+	Foobar.baz a
 end
